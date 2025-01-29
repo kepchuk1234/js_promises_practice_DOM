d9dedcc4a7f2a5b5bb725bd403ae54a9fc417fb7
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-29 14:15:25 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,51 +2,66 @@
 
 const body = document.querySelector('body');
 const firstPromise = new Promise((resolve, reject) => {
+  const timer = setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+
   document.addEventListener('click', () => {
     const text = `First promise was resolved`;
 
+    clearTimeout(timer);
+
     resolve(text);
   });
 
-  setTimeout(() => {
-    reject(new Error('First promise was rejected'));
-  }, 3000);
 });
 const secondPromise = new Promise((resolve) => {
-  document.addEventListener('click', () => {
-    const text = `Second promise was resolved`;
+  let countClick = 0;
 
-    resolve(text);
-  });
+  if (countClick === 0) {
+    document.addEventListener('click', () => {
+      const text = `Second promise was resolved`;
 
-  document.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
+      countClick++;
+      resolve(text);
+    });
 
-    const text = `Second promise was resolved`;
-
-    resolve(text);
-  });
-});
-const thirdPromise = new Promise((resolve, reject) => {
-  document.addEventListener('click', () => {
     document.addEventListener('contextmenu', (e) => {
       e.preventDefault();
 
-      const text = `Third promise was resolved`;
+      const text = `Second promise was resolved`;
 
+      countClick++;
       resolve(text);
     });
-  });
+  }
+});
+const thirdPromise = new Promise((resolve, reject) => {
+  let countClick = 0;
 
-  document.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-
+  if (countClick < 2) {
     document.addEventListener('click', () => {
-      const text = `Third promise was resolved`;
+      document.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
 
-      resolve(text);
+        const text = `Third promise was resolved`;
+
+        countClick++;
+        resolve(text);
+      });
     });
-  });
+
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+
+      document.addEventListener('click', () => {
+        const text = `Third promise was resolved`;
+
+        countClick++;
+        resolve(text);
+      });
+    });
+  }
 });
 
 function messageSuccess(text) {
