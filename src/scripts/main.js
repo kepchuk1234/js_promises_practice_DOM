11ccd39076eea655a7830380a03f0d87d6d95f4b
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-29 14:02:53 +0200

    add promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,72 @@
 'use strict';
+
+const body = document.querySelector('body');
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    const text = `First promise was resolved`;
+
+    resolve(text);
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    const text = `Second promise was resolved`;
+
+    resolve(text);
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+
+    const text = `Second promise was resolved`;
+
+    resolve(text);
+  });
+});
+const thirdPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+
+      const text = `Third promise was resolved`;
+
+      resolve(text);
+    });
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+
+    document.addEventListener('click', () => {
+      const text = `Third promise was resolved`;
+
+      resolve(text);
+    });
+  });
+});
+
+function messageSuccess(text) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.classList.add('success');
+  div.textContent = text;
+  body.append(div);
+}
+
+function messageError(text) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.classList.add('error');
+  div.textContent = text;
+  body.append(div);
+}
+
+firstPromise.then(messageSuccess).catch(messageError);
+secondPromise.then(messageSuccess);
+thirdPromise.then(messageSuccess);
